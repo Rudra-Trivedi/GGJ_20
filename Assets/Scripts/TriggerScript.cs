d50ad45d2111d8e293af44a396cd05f17020fd1d
Rudra-Trivedi <trivedirudra2@gmail.com>
2020-02-01 18:21:58 -0500

    Background

---
+++ Assets/Scripts/TriggerScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject background;
+
+    [SerializeField]
+    private GameObject UpperPivot;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            background.transform.position = UpperPivot.transform.position;
+        }
+    }
+    
+        
+    
+}
