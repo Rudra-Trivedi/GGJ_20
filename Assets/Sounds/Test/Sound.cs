e92d449aaf0ad00970f52488cfdf5aeccaa2c50b
SckrPunch <alvbautista117@gmail.com>
2020-02-01 16:41:46 -0500

    Sound

---
+++ Assets/Sounds/Test/Sound.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sound : MonoBehaviour
+{
+    public AudioSource mainTrack;
+    public AudioSource edTrack;
+    public bool edFlag;
+    public float totalTime;
+    
+    public void PlayMain()
+    {
+        mainTrack.Play();
+    }
+
+    public void PlayED()
+    {
+        Debug.Log("Hello");
+        edTrack.Play();
+    }
+
+    public void StopMain()
+    {
+        mainTrack.Stop();
+    }
+
+    public void StopED()
+    {
+        edTrack.Stop();
+    }
+
+    private void Start()
+    {
+        PlayMain();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown("up"))
+        {
+            StopMain();
+            PlayED();
+        }
+    }
+
+}
