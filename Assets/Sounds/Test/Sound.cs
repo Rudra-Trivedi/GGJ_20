28231331cf1458d3736b4978bdbb357655aa9843
SckrPunch <alvbautista117@gmail.com>
2020-02-02 14:31:36 -0500

    Long sound

--- Assets/Sounds/Test/Sound.cs
+++ Assets/Sounds/Test/Sound.cs
@@ -4,26 +4,52 @@ using UnityEngine;
 
 public class Sound : MonoBehaviour
 {
-    public AudioSource mainTrack;
+    public AudioSource mainTrack1;
+    public AudioSource mainTrack2;
+    //public AudioSource mainBowlTrack1;
+    //public AudioSource mainBowlTrack2;
     public AudioSource edTrack;
     public float totalTime;
     
     
-    public void PlayMain()
+    public void PlayMain1()
     {
-        mainTrack.Play();
-        mainTrack.loop = true;
+        mainTrack1.PlayDelayed(0);
+        mainTrack1.loop = true;
+    }
+    /*
+    public void PlayMain2()
+    {
+        mainTrack2.PlayDelayed(0);
+        mainTrack2.loop = true;
+    }
+    
+    public void PlayBowlMain1()
+    {
+        mainBowlTrack1.PlayDelayed(0);
+        mainBowlTrack1.loop = true;
     }
 
+    public void PlayBowlMain2()
+    {
+        mainBowlTrack2.PlayDelayed(0);
+        mainBowlTrack2.loop = true;
+    }
+    */
     public void PlayED()
     {
         edTrack.Play();
     }
 
-    public void StopMain()
+    public void StopMain1()
     {
-        mainTrack.Stop();
+        mainTrack1.Stop();
     }
+    /*
+    public void StopMain2()
+    {
+        mainTrack2.Stop();
+    }*/
 
     public void StopED()
     {
@@ -32,26 +58,28 @@ public class Sound : MonoBehaviour
 
     private void Start()
     {
-        PlayMain();
+        PlayMain1();
+        //Invoke("PlayMain2", 5.00f); //3.3015
     }
 
     private void Update()
     {
         totalTime += Time.deltaTime;
+        
         //float currentDivis = Mathf.Floor(totalTime / 4.00f);
-        /*
+        
         if(Input.GetKeyDown("up")) //when ED happens
         {
             PlayED();
-            StopMain();
+            StopMain1();
         }
 
         if(Input.GetKeyDown("down")) //when ED is resolved
         {
-            PlayMain();
+            PlayMain1();
             StopED();
         }
-        
+        /*
 
         if(totalTime >= 4.00f)
         {
